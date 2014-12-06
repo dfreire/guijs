1a3f9bfb95bfb6de5e746a0bf259702cb7be003f
dfreire <dario.freire@gmail.com>
2014-12-06 17:49:40 +0000

    update

--- webserver.go
+++ webserver.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/pat"
-	"log"
 	"net/http"
 )
 
@@ -21,7 +20,16 @@ func SignUpHandler(res http.ResponseWriter, req *http.Request) {
 	params := make(map[string]interface{})
 	err := json.NewDecoder(req.Body).Decode(&params)
 	if err != nil {
-		log.Panic(err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	log.Println("++++", params)
+
+	js, err := json.Marshal(params)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(js)
 }
