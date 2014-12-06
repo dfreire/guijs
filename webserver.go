f285ea9c08f85b93223b1b3b2ac2a75dccbcc14c
dfreire <dario.freire@gmail.com>
2014-12-06 18:10:58 +0000

    update

--- webserver.go
+++ webserver.go
@@ -4,16 +4,37 @@ import (
 	"encoding/json"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/pat"
+	"html/template"
 	"net/http"
+	"path"
 )
 
 func main() {
 	router := pat.New()
+
+	router.Get("/", IndexHandler)
+	router.Get("/sign-up", IndexHandler)
+	router.Get("/sign-in", IndexHandler)
+	router.Get("/reset-password", IndexHandler)
+
 	router.Post("/sign-up", SignUpHandler)
 
 	n := negroni.Classic()
 	n.UseHandler(router)
 	n.Run(":3001")
+}
+
+func IndexHandler(res http.ResponseWriter, req *http.Request) {
+	fp := path.Join("templates", "index.html")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(res, nil); err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func SignUpHandler(res http.ResponseWriter, req *http.Request) {
