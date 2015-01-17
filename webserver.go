123511c166ac49d71ba426a4b99780f859b61190
dfreire <dario.freire@gmail.com>
2015-01-17 17:05:28 +0000

    update

--- webserver.go
+++ webserver.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/pat"
 	"html/template"
 	"net/http"
+	"os"
 	"path"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/pat"
+	"github.com/puffinframework/auth"
+	"github.com/puffinframework/config"
+	"github.com/puffinframework/event"
+	"github.com/puffinframework/snapshot"
 )
 
 func main() {
-	router := pat.New()
+	os.Setenv(config.ENV_VAR_NAME, config.MODE_TEST)
+	es := event.NewLeveldbStore()
+	defer es.MustDestroy()
+	ss := snapshot.NewLeveldbStore()
+	defer ss.MustDestroy()
+	authService := auth.NewAuthService(es, ss)
 
+	router := pat.New()
 	router.Get("/", IndexHandler)
 	router.Get("/sign-up", IndexHandler)
 	router.Get("/sign-in", IndexHandler)
 	router.Get("/reset-password", IndexHandler)
-
 	router.Post("/sign-up", SignUpHandler)
 
 	n := negroni.Classic()
@@ -40,6 +51,14 @@ func IndexHandler(res http.ResponseWriter, req *http.Request) {
 func SignUpHandler(res http.ResponseWriter, req *http.Request) {
 	params := make(map[string]interface{})
 	err := json.NewDecoder(req.Body).Decode(&params)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	email := params["email"].(string)
+	password := params["password"].(string)
+	verificationToken, err := authService.SignUp("guijs", email, password)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
