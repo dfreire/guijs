3fc238b6431dcd07612e4ee33c99277547062721
dfreire <dario.freire@gmail.com>
2015-01-22 22:36:38 +0000

    update

--- client/jsx/signin.jsx
+++ client/jsx/signin.jsx
@@ -2,10 +2,24 @@ define(["bus"], function(Bus) {
     var Link = ReactRouter.Link;
 
     var SignIn = React.createClass({
-        handleSignIn: function(e) {
+
+        getInitialState: function() {
+            return { email: "", password: "" };
+        },
+
+        onSignIn: function(e) {
             e.preventDefault();
             console.warn("state:", this.state);
         },
+
+        setEmail: function(e) {
+            this.setState({email: e.target.value});
+        },
+
+        setPassword: function(e) {
+            this.setState({password: e.target.value});
+        },
+
         render: function() {
             return (
                 <div className="my-screen-center">
@@ -17,17 +31,17 @@ define(["bus"], function(Bus) {
                         <h3 className="panel-title">Sign In</h3>
                     </div>
                     <div className="panel-body">
-                        <form role="form" onSubmit={this.handleSignIn}>
+                        <form role="form" onSubmit={this.onSignIn}>
                             <div className="form-group">
                                 <label>Email</label>
-                                <input type="email" className="form-control" ref="email" placeholder="" />
+                                <input type="text" className="form-control" value={this.state.email} onChange={this.setEmail} />
                             </div>
                             <div className="form-group">
                                 <label>Password <Link to="reset-password">(forgot password)</Link></label>
-                                <input type="password" className="form-control" ref="password" placeholder="" />
+                                <input type="password" className="form-control" value={this.state.password} onChange={this.setPassword} />
                             </div>
                             <div className="form-group">
-                                <button href="/sign-in" className="btn btn-primary btn-block">Sign In 2</button>
+                                <button href="/sign-in" className="btn btn-primary btn-block">Sign In</button>
                             </div>
                             <div className="form-group">
                                 <p className="text-center">or</p>
